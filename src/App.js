2409beb812e624a6f4f76cac1f2001a1d04d85d5
taulmav <haloodstboss@live.fr>
2021-10-27 20:27:41 +0200

    changes to fit github

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
       <Header/>
       <Switch>
-        <Route exact path = '/'>
+        <Route path = '/home'>
           <FirstPage/>
         </Route>
         <Route exact path = '/:id'>
