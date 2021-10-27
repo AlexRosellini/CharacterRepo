13b9d238390a3801e5da5508b46cc84aff100e94
taulmav <haloodstboss@live.fr>
2021-10-27 09:21:39 +0200

    Add CSS/Tab/data for Rei.

--- src/Components/Char.js
+++ src/Components/Char.js
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import data from "../Data/database";
 import {useState} from "react";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
 
 
 
@@ -12,7 +14,8 @@ const idparams = useParams()
     console.log(idparams.id)
     const character = data.find(item => item.id == idparams.id)
     console.log(character);
-    const {name, image, short} = character
+    const {name, image, short, Prelude1, Prelude2, Childhood1, Childhood2, Adulthood1} = character
+    const [show, setShow] = useState(false)
 
     return ( 
         <>
@@ -24,7 +27,37 @@ const idparams = useParams()
                     </div>
                 </div>
                 <div className="sheet-right">
-                    
+                    <Tabs>
+                        <TabList className="sheet-right__nav">
+                            <Tab classname='sheet-right__nav-tabs'>Prelude</Tab>
+                            <Tab classname='sheet-right__nav-tabs'>Childhood</Tab>
+                            <Tab classname='sheet-right__nav-tabs'>Adulthood</Tab>
+                            <Tab classname='sheet-right__nav-tabs'>Relationships</Tab>
+                            <Tab classname='sheet-right__nav-tabs'>Npcs</Tab>
+                            <Tab classname='sheet-right__nav-tabs'>Character Sheet</Tab>
+                            <Tab classname='sheet-right__nav-tabs'>Inventory</Tab>
+                        </TabList>
+                        <TabPanel>
+                            <div className="sheet-right__text-content">
+                                <h2>Prelude</h2> 
+                                <p> {Prelude1} </p>
+                                <p> {Prelude2} </p>
+                            </div>  
+                        </TabPanel>
+                        <TabPanel>
+                            <div className="sheet-right__text-content">
+                                <h2>Childhood</h2> 
+                                <p> {Childhood1} </p>
+                                <p> {Childhood2} </p>  
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className="sheet-right__text-content">
+                                <h2>Adulthood</h2> 
+                                <p> {Adulthood1} </p>
+                            </div>
+                        </TabPanel>
+                    </Tabs>
                 </div>
             </div>
         </section>
