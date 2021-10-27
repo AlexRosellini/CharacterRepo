8da2d5dac0fd9dcbdea77d0998646cf954136ba5
taulmav <haloodstboss@live.fr>
2021-10-27 20:46:57 +0200

    test03

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return ( 
         <>
             <header className="header">
+                <Link to='/CharacterRepo/'>
                 <div className="header__title">
                 <div className="header__picture">
                     <img src="https://images-ext-2.discordapp.net/external/wYY9-rZ-KohOepehFEtz9QFHL5G1mQPi3pNZ2OHJqlY/%3Fwidth%3D677%26height%3D677/https/media.discordapp.net/attachments/311192345049825282/314185283950084097/Silverdawn.png" alt="logo" />
                 </div>
                     <h1>Welcome to the TTRPG Repository</h1>
                 </div>
+                </Link>
             </header>
             <div className="text-area">
                 <h2>This Website is meant to store and create the background/character sheets for TTRPGs;
