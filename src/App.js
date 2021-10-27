cff058d6d5dd286f47e75422e1711a40b4e81178
taulmav <haloodstboss@live.fr>
2021-10-27 20:38:17 +0200

    test github againnnn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import FirstPage from "./Components/FirstPage";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from "./Components/Header";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import FirstPage from "./Components/FirstPage";
 import Char from "./Components/Char";
 
 
